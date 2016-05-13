408d11567c98e467ab4d6047e9f9ab3a142c80ee
WilliamGDavis <will@willdavis.net>
2016-05-13 18:30:17 -0400

    Initial Commit

---
+++ PiClock_Admin/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiClock_Admin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Users_Click(object sender, RoutedEventArgs e)
+        {
+            UserManagement um = new UserManagement();
+            um.ShowDialog();
+        }
+
+        private void button_Reports_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
